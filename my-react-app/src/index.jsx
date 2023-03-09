cc2dde75e03543a6dc0f08d018d8c51e0d48a0c7
Soly-M-27 <solymar227@icloud.com>
2023-03-09 19:21:01 -0400

    Removed ExternalPage import

--- my-react-app/src/index.jsx
+++ my-react-app/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MindAR from './public/main.js';
-import ExternalPage from './external-page/ExternalPage.jsx';
 
 function MyComponent() {
     const ar = new MindAR();
